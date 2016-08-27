6b8e784a39da3016d2709dbd9c0bfb191e009337
rec7y33 <rec7y33@hotmail.com>
2016-08-27 20:26:31 +0800

    Error fixed

--- maven-model-builder/src/test/java/org/apache/maven/model/building/DefaultModelBuilderFactoryTest.java
+++ maven-model-builder/src/test/java/org/apache/maven/model/building/DefaultModelBuilderFactoryTest.java
@@ -48,7 +48,7 @@ public class DefaultModelBuilderFactoryTest
         request.setPomFile( getPom( "simple" ) );
 
         ModelBuildingResult result = builder.build( request );
-        assertNull( result );
+        assertNotNull( result );
         assertNotNull( result.getEffectiveModel() );
         assertEquals( "activated", result.getEffectiveModel().getProperties().get( "profile.file" ) );
         Xpp3Dom conf = (Xpp3Dom) result.getEffectiveModel().getBuild().getPlugins().get( 0 ).getConfiguration();
